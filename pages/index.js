7b29678cdb7ef8afe7cb2a3d84043287bd682156
Colin <colin08812@gmail.com>
2024-04-28 01:59:21 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Editor from '../components/playground/editor'
+import Editor from '../components/playground/editor.js'
 
 
 export default function Home() {
